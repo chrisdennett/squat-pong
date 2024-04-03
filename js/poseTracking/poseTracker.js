91f4d1d3a97a9a8af962e0a11460d5316604a49c
Chris Dennett <chrisdennett@gmail.com>
2024-04-03 19:56:58 +0100

    Removed external references

--- js/poseTracking/poseTracker.js
+++ js/poseTracking/poseTracker.js
@@ -51,12 +51,12 @@ export class PoseTracker {
 
   createPoseLandmarker = async (callback) => {
     const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+      "../../libs/@mediapipe/tasks-vision/wasm"
     );
 
     const poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
       baseOptions: {
-        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
+        modelAssetPath: "../../libs/@mediapipe/pose_landmarker_lite.task",
         delegate: "GPU",
       },
       runningMode: this.runningMode,
