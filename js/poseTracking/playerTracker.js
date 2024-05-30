bcb2f83444c0fd66a1cbb118a4f31a15e048add1
Chris Dennett <chrisdennett@gmail.com>
2024-05-30 12:21:21 +0100

    Now working with Move Net rather than Pose Net

--- js/poseTracking/playerTracker.js
+++ js/poseTracking/playerTracker.js
@@ -1,8 +1,8 @@
 export class PlayerTracker {
   constructor() {
     this.landmarks = [];
-    this.minY = 0.35;
-    this.maxY = 0.6;
+    this.minY = 100;
+    this.maxY = 300;
     this.isDetected = false;
   }
 
@@ -33,44 +33,36 @@ export class PlayerTracker {
     this.landmarks = landmarks;
   }
 
-  drawLandmarks(ctx, w, h) {
-    if (this.landmarks.length > 0) {
-      for (let i = 0; i < this.landmarks.length; i++) {
-        let colour = i === 0 ? "red" : "black";
-        const m = this.landmarks[i];
-        ctx.fillStyle = colour;
-        ctx.fillRect(m.x * w, m.y * h, 7, 7);
-      }
-    }
-  }
-
-  drawMinMax(ctx, w, h, isPlayerTwo) {
+  drawMinMax(ctx, w) {
     ctx.fillStyle = "yellow";
-    ctx.fillRect(0, this.minY * h, w, 2);
-    ctx.fillRect(0, this.maxY * h, w, 2);
+    ctx.fillRect(0, this.minY, w, 10);
+    ctx.fillRect(0, this.maxY, w, 10);
   }
 
   get y() {
     const noseY = this.getNoseY();
+
     const range = this.maxY - this.minY;
     const yPos = noseY - this.minY;
 
     return yPos / range;
   }
 
+  // 9 left wrist
   get leftHand() {
     let pos = { x: 0, y: 0, z: 0 };
-    if (this.landmarks && this.landmarks.length >= 17) {
-      pos = this.landmarks[17];
+    if (this.landmarks && this.landmarks.length >= 9) {
+      pos = this.landmarks[9];
     }
 
     return { ...pos };
   }
 
+  // 10 right wrist
   get rightHand() {
     let pos = { x: 0, y: 0, z: 0 };
-    if (this.landmarks && this.landmarks.length >= 18) {
-      pos = this.landmarks[18];
+    if (this.landmarks && this.landmarks.length >= 10) {
+      pos = this.landmarks[10];
     }
 
     return { y: pos.y, visibility: pos.visibility };
--- js/poseTracking/poseTracker.js
+++ js/poseTracking/poseTracker.js
@@ -1,11 +1,6 @@
-// local files setup using npm then copying the files into the right place.
-// npm i @mediapipe/tasks-vision
-// https://josephwritescode.substack.com/p/realtime-web-motion-detection-with-mediapipe
-// https://developers.google.com/mediapipe/solutions/vision/pose_landmarker/index#models
-import * as Vision from "../../libs/@mediapipe/tasks-vision/vision_bundle.mjs";
 import { drawVideoToCanvas } from "../utils/drawVideoToCanvas.js";
 import { PlayerTracker } from "./playerTracker.js";
-const { PoseLandmarker, FilesetResolver, DrawingUtils } = Vision;
+// import * as poseDetection from "../../libs/move-net/pose-detection.js";
 
 export class PoseTracker {
   constructor() {
@@ -16,30 +11,66 @@ export class PoseTracker {
     this.webcamCanvas;
     this.canvas = document.getElementById("poseTrackerCanvas");
     this.canvasCtx = this.canvas.getContext("2d");
-    this.drawingUtils = new DrawingUtils(this.canvasCtx);
     this.videoRunning = false;
     this.logged = false;
     this.segmentationMasks = [];
     this.pose1Canvas = document.getElementById("pose1Canvas");
     this.pose2Canvas = document.getElementById("pose2Canvas");
 
-    // Before we can use PoseLandmarker class we must wait for it to finish
-    // loading. Machine Learning models can be large and take a moment to
-    // get everything needed to run.
-    this.createPoseLandmarker((pl) => {
-      this.poseLandmarker = pl;
-      this.enableWebcam();
-    });
-
     this.landmarks = [];
     this.p1Tracker = new PlayerTracker();
     this.p2Tracker = new PlayerTracker();
 
+    this.poses = [];
+
+    this.detector = null;
+
     this.loop();
+
+    this.enableWebcam();
+    this.setup();
   }
 
-  loop(timeStamp) {
-    this.detectLandmarks(timeStamp);
+  async setup() {
+    this.detector = await poseDetection.createDetector(
+      poseDetection.SupportedModels.MoveNet
+    );
+  }
+
+  async getPoses() {
+    if (this.detector && this.videoRunning) {
+      this.poses = await this.detector.estimatePoses(this.video, {
+        maxPoses: 2,
+        //flipHorizontal: true,
+      });
+
+      // TODO: ensure player 1 landmarks are on the left and p2 on the right
+
+      let p1Landmarks = this.poses.length > 0 ? this.poses[0].keypoints : [];
+      let p2Landmarks = this.poses.length > 1 ? this.poses[1].keypoints : [];
+      this.p1Tracker.setLandmarks(p1Landmarks);
+      this.p2Tracker.setLandmarks(p2Landmarks);
+    }
+    requestAnimationFrame(this.getPoses.bind(this));
+  }
+
+  // Enable the live webcam view and start detection.
+  enableWebcam() {
+    // Activate the webcam stream.
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+      })
+      .then((stream) => {
+        this.video.srcObject = stream;
+        this.video.addEventListener("loadeddata", () => {
+          this.videoRunning = true;
+          this.getPoses();
+        });
+      });
+  }
+
+  loop() {
     this.drawPlayers();
 
     window.requestAnimationFrame(this.loop.bind(this));
@@ -59,7 +90,7 @@ export class PoseTracker {
         this.canvas.height =
           h;
 
-      this.drawLandmarks();
+      this.drawSkeletons(this.webcamCanvas.getContext("2d"));
       const ctx1 = this.pose1Canvas.getContext("2d");
       const ctx2 = this.pose2Canvas.getContext("2d");
 
@@ -69,18 +100,8 @@ export class PoseTracker {
       ctx2.drawImage(this.webcamCanvas, 0, 0, w, h, 0, 0, w, h);
       ctx2.drawImage(this.canvas, 0, 0, w, h, 0, 0, w, h);
 
-      this.p1Tracker.drawMinMax(
-        ctx1,
-        this.pose1Canvas.width,
-        this.pose1Canvas.height,
-        false
-      );
-      this.p2Tracker.drawMinMax(
-        ctx2,
-        this.pose2Canvas.width,
-        this.pose2Canvas.height,
-        true
-      );
+      this.p1Tracker.drawMinMax(ctx1, this.pose1Canvas.width);
+      this.p2Tracker.drawMinMax(ctx2, this.pose2Canvas.width);
     }
   }
 
@@ -92,129 +113,154 @@ export class PoseTracker {
     return this.video.videoHeight;
   }
 
-  createPoseLandmarker = async (callback) => {
-    const vision = await FilesetResolver.forVisionTasks(
-      "../../libs/@mediapipe/tasks-vision/wasm"
-    );
+  // detectLandmarks(timeStamp) {
+  //   if (!this.poseLandmarker || !this.videoRunning || !this.webcamCanvas)
+  //     return;
 
-    const poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
-      baseOptions: {
-        modelAssetPath: "../../libs/@mediapipe/pose_landmarker_lite.task",
-        delegate: "GPU",
-      },
-      runningMode: "VIDEO",
-      numPoses: 2,
-      minPoseDetectionConfidence: 0.8,
-      minPosePresenceConfidence: 0.8,
-      minTrackingConfidence: 0.8,
-      outputSegmentationMasks: false,
-    });
+  //   if (this.lastVideoTime !== this.video.currentTime) {
+  //     this.lastVideoTime = this.video.currentTime;
+  //     this.poseLandmarker.detectForVideo(
+  //       this.webcamCanvas,
+  //       timeStamp,
+  //       (result) => {
+  //         this.landmarks = result.landmarks;
 
-    callback(poseLandmarker);
-  };
+  //         let p1Landmarks = [];
+  //         let p2Landmarks = [];
 
-  // Enable the live webcam view and start detection.
-  enableWebcam() {
-    if (!this.poseLandmarker) {
-      console.log("Wait! poseLandmaker not loaded yet.");
-      return;
-    }
+  //         // order find poses might not be same as player 1 then player 2
+  //         const firstPoseFound = this.landmarks.length > 0;
+  //         const secondPoseFound = this.landmarks.length > 1;
 
-    // Activate the webcam stream.
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-      })
-      .then((stream) => {
-        this.video.srcObject = stream;
-        this.video.addEventListener("loadeddata", () => {
-          this.videoRunning = true;
-        });
-      });
-  }
+  //         // set first pose to p1 or p2 depending on side found
+  //         if (firstPoseFound) {
+  //           const noseX = this.landmarks[0][0].x * this.width;
+  //           // if on left set it to p1
+  //           if (noseX < this.width / 2) {
+  //             p2Landmarks = this.landmarks[0];
+  //           } else {
+  //             p1Landmarks = this.landmarks[0];
+  //           }
+  //         }
 
-  detectLandmarks(timeStamp) {
-    if (!this.poseLandmarker || !this.videoRunning || !this.webcamCanvas)
-      return;
+  //         // set second pose to p1 or p2 depending on side found
+  //         if (secondPoseFound) {
+  //           const noseX = this.landmarks[1][0].x * this.width;
+  //           // if on left set it to p1
+  //           if (noseX < this.width / 2) {
+  //             p2Landmarks = this.landmarks[1];
+  //           } else {
+  //             p1Landmarks = this.landmarks[1];
+  //           }
+  //         }
 
-    if (this.lastVideoTime !== this.video.currentTime) {
-      this.lastVideoTime = this.video.currentTime;
-      this.poseLandmarker.detectForVideo(
-        this.webcamCanvas,
-        timeStamp,
-        (result) => {
-          this.landmarks = result.landmarks;
-
-          // if (!this.logged && result.segmentationMasks.length > 0) {
-          //   this.logged = true;
-          //   console.log("result: ", result);
-          // }
-          // this.segmentationMasks = result.segmentationMasks;
-
-          let p1Landmarks = [];
-          let p2Landmarks = [];
-
-          // order find poses might not be same as player 1 then player 2
-          const firstPoseFound = this.landmarks.length > 0;
-          const secondPoseFound = this.landmarks.length > 1;
-
-          // set first pose to p1 or p2 depending on side found
-          if (firstPoseFound) {
-            const noseX = this.landmarks[0][0].x * this.width;
-            // if on left set it to p1
-            if (noseX < this.width / 2) {
-              p2Landmarks = this.landmarks[0];
-            } else {
-              p1Landmarks = this.landmarks[0];
-            }
-          }
-
-          // set second pose to p1 or p2 depending on side found
-          if (secondPoseFound) {
-            const noseX = this.landmarks[1][0].x * this.width;
-            // if on left set it to p1
-            if (noseX < this.width / 2) {
-              p2Landmarks = this.landmarks[1];
-            } else {
-              p1Landmarks = this.landmarks[1];
-            }
-          }
-
-          this.p1Tracker.setLandmarks(p1Landmarks);
-          this.p2Tracker.setLandmarks(p2Landmarks);
-        }
-      );
-    }
-  }
+  //         this.p1Tracker.setLandmarks(p1Landmarks);
+  //         this.p2Tracker.setLandmarks(p2Landmarks);
+  //       }
+  //     );
+  //   }
+  // }
 
   getVideo() {
     return this.video;
   }
 
-  drawLandmarks() {
-    // const ctx = this.canvas.getContext("2d");
-    // ctx.globalAlpha = 0.4;
-    // ctx.drawImage(this.video, 0, 0);
-    // ctx.globalAlpha = 1;
+  drawSkeletons(ctx) {
+    ctx.strokeStyle = "yellow";
+    ctx.lineWidth = 10;
+    let pose, partA, partB;
 
-    // if (this.segmentationMasks.length > 0) {
-    //   const offscreenCanvas = this.segmentationMasks[0].canvas;
+    // Draw all the tracked landmark points
+    for (let i = 0; i < this.poses.length; i++) {
+      pose = this.poses[i];
+      // shoulder to wrist
+      for (let j = 5; j < 9; j++) {
+        if (
+          pose.keypoints[j].score > 0.1 &&
+          pose.keypoints[j + 2].score > 0.1
+        ) {
+          partA = pose.keypoints[j];
+          partB = pose.keypoints[j + 2];
+          this.drawLine(ctx, partA, partB);
+        }
+      }
+      // shoulder to shoulder
+      partA = pose.keypoints[5];
+      partB = pose.keypoints[6];
+      if (partA.score > 0.1 && partB.score > 0.1) {
+        this.drawLine(ctx, partA, partB);
+      }
+      // hip to hip
+      partA = pose.keypoints[11];
+      partB = pose.keypoints[12];
+      if (partA.score > 0.1 && partB.score > 0.1) {
+        this.drawLine(ctx, partA, partB);
+      }
+      // shoulders to hips
+      partA = pose.keypoints[5];
+      partB = pose.keypoints[11];
+      if (partA.score > 0.1 && partB.score > 0.1) {
+        this.drawLine(ctx, partA, partB);
+      }
+      partA = pose.keypoints[6];
+      partB = pose.keypoints[12];
+      if (partA.score > 0.1 && partB.score > 0.1) {
+        this.drawLine(ctx, partA, partB);
+      }
+      // hip to foot
+      for (let j = 11; j < 15; j++) {
+        if (
+          pose.keypoints[j].score > 0.1 &&
+          pose.keypoints[j + 2].score > 0.1
+        ) {
+          partA = pose.keypoints[j];
+          partB = pose.keypoints[j + 2];
+          this.drawLine(ctx, partA, partB);
+        }
+      }
 
-    //   const bitmapOne =
-    //     this.segmentationMasks[0].canvas.transferToImageBitmap();
-
-    //   ctx.drawImage(bitmapOne, 0, 0);
-    // }
-
-    for (const landmark of this.landmarks) {
-      const radius = DrawingUtils.lerp(landmark[0].z, -0.15, 0.1, 5, 1);
-      // const radius = 10;
-      this.drawingUtils.drawLandmarks(landmark, { radius });
-
-      this.drawingUtils.drawConnectors(
-        landmark,
-        PoseLandmarker.POSE_CONNECTIONS
-      );
+      // draw face
+      this.drawCircle(ctx, pose.keypoints[0]); // nose
+      this.drawCircle(ctx, pose.keypoints[1]); // left eye
+      this.drawCircle(ctx, pose.keypoints[2]); // right eye
+      this.drawCircle(ctx, pose.keypoints[3]); // left ear
+      this.drawCircle(ctx, pose.keypoints[4]); // right ear
     }
   }
+
+  drawLine(ctx, from, to) {
+    this.drawCircle(ctx, from);
+    this.drawCircle(ctx, to);
+
+    ctx.beginPath();
+    ctx.moveTo(from.x, from.y);
+    ctx.lineTo(to.x, to.y);
+    ctx.stroke();
+  }
+
+  drawCircle(ctx, pt) {
+    ctx.beginPath();
+    ctx.arc(pt.x, pt.y, 5, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 }
+
+/* Points (view on left of screen = left part - when mirrored)
+    0 nose
+    1 left eye
+    2 right eye
+    3 left ear
+    4 right ear
+    5 left shoulder
+    6 right shoulder
+    7 left elbow
+    8 right elbow
+    9 left wrist
+    10 right wrist
+    11 left hip
+    12 right hip
+    13 left kneee
+    14 right knee
+    15 left foot
+    16 right foot
+  */
