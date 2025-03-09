3f2a0af73c4b7776e92840b3045601a22ff4f8be
Chris Dennett <chrisdennett@gmail.com>
2025-03-09 08:01:53 +0000

    Added all landmarks

--- js/poseTracking/stickFigureRenderer.js
+++ js/poseTracking/stickFigureRenderer.js
@@ -2,6 +2,7 @@ export class StickFigureRenderer {
   constructor(canvasElement) {
     this.canvas = canvasElement;
     this.ctx = this.canvas.getContext("2d");
+    this.lineWidth = 10;
   }
 
   clear() {
@@ -13,31 +14,36 @@ export class StickFigureRenderer {
 
     // Scale factors to map from video coordinates to canvas dimensions
     // Assuming landmarks are in the range of 0-640 for x and 0-480 for y (typical webcam resolution)
-    const scaleX = this.canvas.width / 640;
-    const scaleY = this.canvas.height / 480;
+    this.scaleX = this.canvas.width / 640;
+    this.scaleY = this.canvas.height / 480;
 
     this.ctx.strokeStyle = color;
-    this.ctx.lineWidth = 4;
+    this.ctx.lineWidth = this.lineWidth;
 
     // Draw head (circle around nose)
     const nose = landmarks[0];
-    // this.ctx.strokeRect(nose.x, nose.y, 100, 100);
-    if (nose) {
-      const x = nose.x * scaleX;
-      const y = nose.y * scaleY;
+    const leftEye = landmarks[1];
+    const rightEye = landmarks[2];
+    const leftEar = landmarks[3];
+    const rightEar = landmarks[4];
 
-      this.ctx.beginPath();
-      this.ctx.arc(x, y, 15, 0, 2 * Math.PI);
-      this.ctx.stroke();
+    for (let i = 0; i < landmarks.length; i++) {
+      this.drawCircle(landmarks[i]);
     }
+
+    this.drawCircle(nose);
+    this.drawCircle(leftEye);
+    this.drawCircle(rightEye);
+    this.drawCircle(leftEar);
+    this.drawCircle(rightEar);
 
     // Modify drawLine to handle normalization
     const drawLine = (pointA, pointB) => {
       if (pointA && pointB) {
-        const x1 = pointA.x * scaleX;
-        const y1 = pointA.y * scaleY;
-        const x2 = pointB.x * scaleX;
-        const y2 = pointB.y * scaleY;
+        const x1 = pointA.x * this.scaleX;
+        const y1 = pointA.y * this.scaleY;
+        const x2 = pointB.x * this.scaleX;
+        const y2 = pointB.y * this.scaleY;
 
         this.ctx.beginPath();
         this.ctx.moveTo(x1, y1);
@@ -63,5 +69,14 @@ export class StickFigureRenderer {
     drawLine(landmarks[13], landmarks[15]); // Left knee to left ankle
     drawLine(landmarks[12], landmarks[14]); // Right hip to right knee
     drawLine(landmarks[14], landmarks[16]); // Right knee to right ankle
+  }
+
+  drawCircle(pt) {
+    const x = pt.x * this.scaleX;
+    const y = pt.y * this.scaleY;
+
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, 15, 0, 2 * Math.PI);
+    this.ctx.stroke();
   }
 }
